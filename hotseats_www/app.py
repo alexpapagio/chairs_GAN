ac71a2d6afc7abb5ea37ec319c96ac3b7151342e
Amin Diafi <amindiafi.ad@gmail.com>
2024-03-30 16:20:10 +0000

    Horizintal interp. layout

--- hotseats_www/app.py
+++ hotseats_www/app.py
@@ -4,23 +4,17 @@ and display a new image combining the two images.
 """
 
 import streamlit as st
-
 import uploaded_images, autoencoder
 import numpy as np
-
 import tensorflow as tf
 
-
-# set to a local path as needed
+# Set to a local path as needed
 DATASET_VIEWER_DEFAULT_IMAGE_DIRECTORY = None
 DATASET_VIEWER_NUM_IMAGES = 10
 
-
 def playback_uploaded_image(img_ref: str, default: str = None):
     """img_ref is e.g. 'one' or 'A'"""
-    uploaded_file = st.file_uploader(
-        f"Choose file {img_ref}", type=["png", "jpg", "jpeg"]
-    )
+    uploaded_file = st.file_uploader(f"Choose file {img_ref}", type=["png", "jpg", "jpeg"])
     if uploaded_file is None:
         if default is not None:
             st.markdown(f"### Default image {img_ref}")
@@ -29,10 +23,6 @@ def playback_uploaded_image(img_ref: str, default: str = None):
         else:
             return None
     else:
-        # Streamlit base64 encodes the images for us, we can give it the in-memory
-        # image data.
-        # Let's check it's really an image first, not a renamed text file etc.
-        # Streamlit only checks the file extension.
         if uploaded_images.is_image(uploaded_file):
             st.markdown(f"### Uploaded image {img_ref}")
             st.image(uploaded_file)
@@ -40,48 +30,33 @@ def playback_uploaded_image(img_ref: str, default: str = None):
         else:
             st.error("Uploaded file is unsupported.")
 
-
 def main():
     """
     Main function of the Streamlit app.
     """
-
     # Check Streamlit version
     if st.__version__ != "1.29.0":
-        st.warning(
-            "File upload may not work with this version of Streamlit. Please install version 1.29.0."
-        )
+        st.warning("File upload may not work with this version of Streamlit. Please install version 1.29.0.")
 
-    st.set_page_config(
-        page_title="Hot Seats",
-        page_icon=":chair:",
-        layout="centered",
-        initial_sidebar_state="expanded",
-        menu_items={"About": "# This is a header. This is a hot seats app!"},
-    )
+    st.set_page_config(page_title="Hot Seats",
+                       page_icon=":chair:",
+                       layout="centered",
+                       initial_sidebar_state="expanded",
+                       menu_items={"About": "# This is a header. This is a hot seats app!"})
 
-    # display the tensorflow keras version
+    # Display the TensorFlow Keras version
     st.markdown(f"TensorFlow version: {tf.__version__}")
 
     col1, col2 = st.columns(2)
     with col1:
         st.title("_Hot_ Seats :fire: :seat:")
     with col2:
-        st.image(
-            "https://storage.googleapis.com/chairs-gan-images/Hotseats-logo.webp",
-            use_column_width="auto",
-        )
+        st.image("https://storage.googleapis.com/chairs-gan-images/Hotseats-logo.webp", use_column_width="auto")
 
     # Gradient divider
-    st.markdown(
-        """
+    st.markdown("""
     <hr style="height: 2px; border: none; background: linear-gradient(to right, red, gray, white);"/>
-    """,
-        unsafe_allow_html=True,
-    )
-
-    # import dataset_viewer
-    # dataset_viewer.render_image_dataset_viewer()
+    """, unsafe_allow_html=True)
 
     st.header("_PNG_ Chair Image Uploader")
 
@@ -89,15 +64,11 @@ def main():
 
     with col1:
         st.subheader("Chair one :fire: :seat:")
-        img_a = playback_uploaded_image(
-            "Choose file one", default="static/default_a.png"
-        )
+        img_a = playback_uploaded_image("one", default="static/default_a.png")
 
     with col2:
         st.subheader("Chair two :fire: :seat: :seat:")
-        img_b = playback_uploaded_image(
-            "Choose file two", default="static/default_b.png"
-        )
+        img_b = playback_uploaded_image("two", default="static/default_b.png")
 
     if img_a is None or img_b is None:
         st.warning("Please upload two chair images.")
@@ -107,33 +78,21 @@ def main():
     encoder = autoencoder.encoder_model()
     decoder = autoencoder.decoder_model()
 
-    # TODO: preprocess the uploads
-    _, _, img_a_latent_vector = encoder.predict(
-        np.array([uploaded_images.preprocess_image(img_a)])
-    )
-    _, _, img_b_latent_vector = encoder.predict(
-        np.array([uploaded_images.preprocess_image(img_b)])
-    )
+    _, _, img_a_latent_vector = encoder.predict(np.array([uploaded_images.preprocess_image(img_a)]))
+    _, _, img_b_latent_vector = encoder.predict(np.array([uploaded_images.preprocess_image(img_b)]))
 
-    # st.markdown("### Latent vector A")
-    # st.write(img_a_latent_vector)
-    # st.markdown("### Latent vector B")
-    # st.write(img_b_latent_vector)
+    interpolated_latent_vectors = autoencoder.interpolate_latent_vectors(img_a_latent_vector, img_b_latent_vector, steps=10)
 
-    # Interpolate the latent vectors
-    interpolated_latent_vectors = autoencoder.interpolate_latent_vectors(
-        img_a_latent_vector, img_b_latent_vector, steps=10
-    )
-    # st.write(interpolated_latent_vectors[0])
-
-    # Decode the interpolated encodings
-    for interpolated_encoding in interpolated_latent_vectors:
-        interpolated_encoding_reshaped = interpolated_encoding.reshape(
-            (1, 100)
-        )  # Reshape to (1, 100)
+    # Displaying images in one row
+    cols = st.columns(len(interpolated_latent_vectors))
+    for col, interpolated_encoding in zip(cols, interpolated_latent_vectors):
+        interpolated_encoding_reshaped = interpolated_encoding.reshape((1, 100))  # Reshape to (1, 100)
         reconstructed_image = decoder.predict(interpolated_encoding_reshaped)
-        st.image(reconstructed_image)
+        col.image(reconstructed_image, use_column_width=True)
 
+    # Expandable details section
+    with st.expander("See Details"):
+        st.write("Here we can have a drop down list, to choose which image to send into ecom search?")
 
 if __name__ == "__main__":
     main()
