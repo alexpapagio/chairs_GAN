7e7fecbd8dc5ce9a08cc4405a182fe418dc620e3
alexandrospapageorgiou <alexpapagio@gmail.com>
2024-04-02 22:59:38 +0100

    update on shopping page

--- hotseats_www/pages/shopping.py
+++ hotseats_www/pages/shopping.py
@@ -35,25 +35,42 @@ def shopping_page():
     # Display the selected interpolated image larger
     st.image(interpolation_paths[selected_index], caption=f"Enlarged {selected_interpolation}", width=500)
 
-    # Perform SerpAPI search for Interpolation 4 details using a specific URL
-    if selected_interpolation == "Interpolation 4":
-        params = {
-            "engine": "google_lens",
-            "url": "https://github.com/alexpapagio/test1/blob/main/intepolation-3.png?raw=true",
-            "api_key": st.secrets["SERPAPI_KEY"],  # Use your actual SERPAPI_KEY
-        }
+    # Create a dictionary with interpolation options and filenames
+    select = {
+        'Interpolation 1': 'static/interpolations/intepolation-0.png',
+        'Interpolation 2': 'static/interpolations/intepolation-1.png',
+        'Interpolation 3': 'static/interpolations/intepolation-2.png',
+        'Interpolation 4': 'static/interpolations/intepolation-3.png',
+        'Interpolation 5': 'static/interpolations/intepolation-4.png',
+        'Interpolation 6': 'static/interpolations/intepolation-5.png',
+        'Interpolation 7': 'static/interpolations/intepolation-6.png',
+        'Interpolation 8': 'static/interpolations/intepolation-7.png',
+        'Interpolation 9': 'static/interpolations/intepolation-8.png',
+        'Interpolation 10': 'static/interpolations/intepolation-9.png'
+    }
 
-        search = GoogleSearch(params)
-        results = search.get_dict()
 
-        if "visual_matches" in results:
-            st.header(f"Details for {selected_interpolation}:")
-            for match in results["visual_matches"][0:3]:  # Display details for the first 3 matches
-                st.image(match["thumbnail"], caption=match["title"])
-                st.markdown(f"Source: {match['source']}")
-                st.markdown(f"[View Source]({match['link']})")
-        else:
-            st.error("No visual matches found for Interpolation 4.")
+    # Get the filename of the selected interpolation
+    selected_filename = select[selected_interpolation]
+
+    # Perform SerpAPI search for the selected interpolation details using the specific filename
+    params = {
+        "engine": "google_lens",
+        "url": f"https://github.com/alexpapagio/chairs_GAN/blob/master/hotseats_www/{selected_filename}?raw=true",
+        "api_key": st.secrets["SERPAPI_KEY"],  # Use your actual SERPAPI_KEY
+    }
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    if "visual_matches" in results:
+        st.header(f"Shopping results for {selected_interpolation}:")
+        for match in results["visual_matches"][0:3]:  # Display details for the first 3 matches
+            st.image(match["thumbnail"], caption=match["title"])
+            st.markdown(f"Source: {match['source']}")
+            st.markdown(f"[View Source]({match['link']})")
+    else:
+        st.error(f"No visual matches found for {selected_interpolation}.")
+
+
 
 if __name__ == "__main__":
     shopping_page()
